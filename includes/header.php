b256ff75b94a94b61824571e172944e99c7dbfa7
Jerad Beauregard <jeradbeauregard@gmail.com>
2025-07-01 13:37:03 -0400

    header html

---
+++ includes/header.php
@@ -0,0 +1,17 @@
+<link rel="stylesheet" href="/jim-prototype/public/assets/css/header.css">
+<header id="header_container">
+    <div id="logo_container">
+        <a href="/" class="logo">Jim</a>
+    </div>
+    <div id="nav_container">
+        <nav>
+            <ul>
+                <li><a href="#">My Routines</a></li>
+                <li><a href="#">My Exercises</a></li>
+                <li><a href="#">Routine Builder</a></li>
+                <li><a href="#">Explore Exercises</a></li>
+                <li><a href="#">Create Exercise</a></li>
+            </ul>
+        </nav>
+    </div>
+</header>
---
+++ public/routinebuilder.php
@@ -0,0 +1,5 @@
+<!-- base body styles -->
+<link rel="stylesheet" href="/jim-prototype/public/assets/css/header.css"> 
+<?php
+include '../includes/header.php';
+?>
