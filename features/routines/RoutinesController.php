04de3281a350846a14ce9b227b113a7ac02902ef
Jerad Beauregard <jeradbeauregard@gmail.com>
2025-07-05 12:12:10 -0400

    temp routine session set up

--- features/routines/RoutinesController.php
+++ features/routines/RoutinesController.php
@@ -1,8 +1,34 @@
 
 <?php
-include(__DIR__ . '/../../includes/db.php');                // ✅ db is in /includes
-include(__DIR__ . '/RoutinesService.php');                  // ✅ service is in same folder
+session_start();
+
+include(__DIR__ . '/../../includes/db.php');                
+include(__DIR__ . '/RoutinesService.php');                 
 
 $exercises = getAllExercises($pdo);
 
+
+
+// add exercise to temp routine logic
+
+if(!isset($_SESSION['temp_routine'])){
+    $_SESSION['temp_routine'] = []; // if temp routine isnt set create new array  
+
+}
+
+if($_SERVER['REQUEST_METHOD'] === 'POST' && isset($_POST['add_exercise'])){
+
+    $exercise_id = $_POST['exercise_id'];
+ //prevent duplicate exercises in routine -- may consider changing this later if user wants exercise multiple times in routine
+    if(!in_array($exercise_id,$_SESSION['temp_routine'])){
+        $_SESSION['temp_routine'][] = $exercise_id; // adds exercise to session array
+    }
+
+}
+
+$temp_routine = $_SESSION['temp_routine'];
+$temp_exercises = getTempExercises($pdo, $temp_routine);
+
 include(__DIR__ . '/routine_builder.php');
+
+?>
--- features/routines/RoutinesService.php
+++ features/routines/RoutinesService.php
@@ -1,10 +1,20 @@
 <?php
-
+// get all exercises
 function getAllExercises($pdo) {
     $statement = $pdo->prepare("SELECT * FROM exercises");
     $statement->execute();
     return $statement->fetchAll(PDO::FETCH_ASSOC); //  get all rows, as associative arrays
 }
 
+// get exercises from array (temp_routines)
+
+function getTempExercises($pdo,$temp_routine){
+
+    $placeholders = implode(',', array_fill(0, count($temp_routine), '?'));
+    $statement = $pdo->prepare("SELECT * FROM exercises WHERE id in ($placeholders)");
+    $statement->execute($temp_routine);
+    $exercises = $statement->fetchAll();
+    return $exercises;
+}
 
 ?>
